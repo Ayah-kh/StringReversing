8a3886a476e4320aaa32582d73dd3f4785610d44
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-15 11:45:54 +0200

    First Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+class Reverse {
+    public static void reverseChars(String word){
+        char[] chars = word.toCharArray();
+        for (int i=chars.length;i>0;i--){
+            System.out.print(chars[i-1]);
+        }
+//        return word;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Reverse.reverseChars("Hello");
+
     }
 }
