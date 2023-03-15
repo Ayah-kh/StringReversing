7cc95c33562c2c07b6a4c0641dbe3597a167c7ff
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-15 15:20:45 +0200

    First Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,33 +3,35 @@ package org.example;
 import java.util.Arrays;
 
 class Reverse {
-    public static String reverseChars(String word){
+    public static String reverseChars(String word) {
         char[] chars = word.toCharArray();
-        char[] revChars=new char[chars.length];
-        for (int i=chars.length;i>0;i--){
-            revChars[chars.length-i]=chars[i-1];
+        char[] revChars = new char[chars.length];
+        for (int i = chars.length; i > 0; i--) {
+            revChars[chars.length - i] = chars[i - 1];
         }
         return String.valueOf(revChars);
     }
 
     public static String reverseWords(String word) {
         String[] words = word.split(" ");
-        String[] revWords=new String[words.length];
-        for (int i=words.length;i>0;i--){
-            revWords[words.length-i]=words[i-1];
+        String[] revWords = new String[words.length];
+        for (int i = words.length; i > 0; i--) {
+            revWords[words.length - i] = words[i - 1];
         }
-        String string1 = Arrays.toString(revWords);
-        String string= string1.replace("[","")
-                .replace("]","")
-                .replace(',',' ');
-        return string;
+
+        return Arrays.toString(revWords)
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", "");
     }
+
 }
 
 public class Main {
     public static void main(String[] args) {
         String revChar = Reverse.reverseChars("Hello world");
         String revWord = Reverse.reverseWords("Hello world kids people");
+
         System.out.println(revChar);
         System.out.println(revWord);
 
